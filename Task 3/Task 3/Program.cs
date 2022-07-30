21aac9e928324a6ca7ea1303bc7851c817949091
kally1111 <83878434+kally1111@users.noreply.github.com>
2022-07-30 14:50:42 +0300

    Add files via upload

---
+++ Task 3/Task 3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+namespace Task_3
+{
+    class Program
+    {
+        static void Main()
+        {
+            int totalPoints = 3_000_000;
+            int a = 0;
+            int pointsCircle = 0;
+            Random rnd = new Random();
+            double min = -0.5;
+            double max = 0.5;
+            while (a<=totalPoints)
+            {
+                double x = rnd.NextDouble() * (max - min) + min; 
+                double y = rnd.NextDouble() * (max - min) + min;
+                double distance = Math.Sqrt(x * x + y * y);
+
+                if (distance<=max)
+                {
+                    pointsCircle++;
+                }
+                a++;
+            }
+
+            double pi=4.0*pointsCircle / totalPoints ;
+            Console.Write($"Pi is: {Math.Round(pi, 15)}");
+        }
+
+    }
+}
